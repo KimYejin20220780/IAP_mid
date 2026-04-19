db779570ae71795fca173d567316a2a7216abca0
lftf <265744312+KimYejin20220780@users.noreply.github.com>
2026-04-19 14:37:18 +0000

    8

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let map;
+let pacX;
 
 function preload(){
   map = loadImage("Map.png");
@@ -8,12 +9,19 @@ function preload(){
 function setup() {
   createCanvas(2816, 1536);
   background(0);
-  image(map, 0, 0);
 }
 
 function draw() {
+  image(map, 0, 0);
   noStroke();
   fill(255, 255, 0);
   arc(300, 770, 60, 60, PI/4.5, PI/0.55);
 
+}
+
+function keyPressed(){
+    background(0);
+    if(key==="d"){
+        pacX = pacX+10;
+    }
 }
