43ccc5314907685bfd3d7275c3f0d8c52f2cd295
lftf <265744312+KimYejin20220780@users.noreply.github.com>
2026-04-19 14:32:30 +0000

    6

--- sketch.js
+++ sketch.js
@@ -7,12 +7,13 @@ function preload(){
 
 function setup() {
   createCanvas(2816, 1536);
+  background(0);
+  image(map, 0, 0);
 }
 
 function draw() {
-  image(map, 0, 0);
-  fill(255, 255, 0)
-  arc(250, 250, 190, 190, PI/4.5, PI/0.55);
-
+  noStroke();
+  fill(255, 255, 0);
+  arc(250, 250, 100, 100, PI/4.5, PI/0.55);
 }
 
